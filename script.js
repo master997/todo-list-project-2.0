29f403917de7d7e23a94c8fbbde6b8837eaf35ab
Mohammed Master <mohammedmaster1793@gmail.com>
2024-12-17 14:42:26 +0000

    adding task section

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+//DOM Elements
+const taskInput = document.querySelector(".task-input");
+const taskBTN = document.querySelector(".add-task");
+const taskList = document.querySelector(".task-list");
+
+const addTask = function () {
+  const taskValue = taskInput.value; //gets value from input
+  const newElement = document.createElement("li"); //creates new element list item
+  newElement.textContent = taskValue; //gets task content from task value
+  taskList.appendChild(newElement); //adds element to the task list
+  taskInput.value = ""; //clears input
+};
+taskBTN.addEventListener("click", function () {
+  console.log("button clicked");
+  addTask();
+});
