edd6afa99071dc3a52be656dcacabc5c32d258fd
Mohammed Master <mohammedmaster1793@gmail.com>
2024-12-19 12:16:31 +0000

    progress

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const taskInput = document.querySelector(".task-input");
 const taskBTN = document.querySelector(".add-task");
 const taskList = document.querySelector(".task-list");
 
-let tasks = []; //empty array for local storage
+let tasks = []; //gloabl array for local storage
 const addTask = function () {
   if (taskInput.value === "") {
     // if no value given alert is given to make sure a a proper task is added
@@ -16,15 +16,20 @@ const addTask = function () {
     const newElement = document.createElement("li"); //creates a new list element
     newElement.textContent = taskValue; //sets new element text to the value of the task input
     taskList.appendChild(newElement); //adds new list item to the to the task list
-    tasks.push(taskValue); //adds task to empty array
+    tasks.push(taskValue); // push task inout value into array
+
+    saveTask();
     console.log(tasks);
     taskInput.value = ""; //clears input
-    saveTasks();
+
+    const deleteBTN = document.createElement("button");
+    deleteBTN.textContent = "Delete Task";
+    deleteBTN.classList.add("delete-btn");
+    newElement.appendChild(deleteBTN);
   }
 };
-
-const saveTasks = function () {
-  localStorage.setItem("tasks", JSON.stringify(tasks)); //saves the array and puts it into a string
+const saveTask = function () {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 taskBTN.addEventListener("click", function () {
   addTask();
